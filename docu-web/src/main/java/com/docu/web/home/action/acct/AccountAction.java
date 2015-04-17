2b45f823d58527cc3c95791f662bec0f2a1a9979
MarquisHu <marquis_hu@163.com>
2015-04-17 23:20:34 +0800

    Add some new menu item

---
+++ docu-web/src/main/java/com/docu/web/home/action/acct/AccountAction.java
@@ -0,0 +1,14 @@
+package com.docu.web.home.action.acct;
+
+import com.alibaba.citrus.turbine.Context;
+import com.alibaba.citrus.turbine.TurbineRunData;
+
+public class AccountAction {
+
+	public void doSaveAccount(TurbineRunData rundata, Context context){
+		
+	}
+	public void doAddAccount(TurbineRunData rundata, Context context){
+		
+	}
+}
---
+++ docu-web/src/main/java/com/docu/web/home/action/user/UserAction.java
@@ -0,0 +1,14 @@
+package com.docu.web.home.action.user;
+
+import com.alibaba.citrus.turbine.Context;
+import com.alibaba.citrus.turbine.TurbineRunData;
+
+public class UserAction {
+	
+	public void doSaveUser(TurbineRunData rundata, Context context){
+		
+	}
+	public void doAddUser(TurbineRunData rundata, Context context){
+		
+	}
+}
