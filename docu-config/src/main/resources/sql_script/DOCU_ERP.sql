1875b73072d474e01e658ad5dd364aea64a8f5f7
MarquisHu <marquis_hu@163.com>
2015-04-22 22:51:14 +0800

    Add some new table

--- docu-config/src/main/resources/sql_script/DOCU_ERP.sql
+++ docu-config/src/main/resources/sql_script/DOCU_ERP.sql
@@ -1,8 +1,9 @@
 /*
-SQLyog ‰ºÅ‰∏öÁâà - MySQL GUI v8.14 
+SQLyog ∆Û“µ∞Ê - MySQL GUI v8.14 
 MySQL - 5.6.16 : Database - docu_erp
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -35,21 +36,48 @@ CREATE TABLE `docu_erp_account` (
 
 /*Data for the table `docu_erp_account` */
 
-insert  into `docu_erp_account`(`ACCOUNT_ID`,`USER_ID`,`BALANCE_AMOUNT`,`PRIVATE_AMOUNT`,`COMMON_AMOUNT`,`UPDATE_BY`,`UPDATE_TIME`) values ('1','HUMA4',300,120,180,'HUMA4','2015-04-19 21:27:04'),('2','ADMIN',300,120,180,'HUMA4','2015-04-19 21:34:42');
+insert  into `docu_erp_account`(`ACCOUNT_ID`,`USER_ID`,`BALANCE_AMOUNT`,`PRIVATE_AMOUNT`,`COMMON_AMOUNT`,`UPDATE_BY`,`UPDATE_TIME`) values ('1','HUMA4',400,80,320,'huma4','2015-04-22 17:00:15'),('2','ADMIN',100,20,80,'huma4','2015-04-22 12:46:43');
 
-/*Table structure for table `docu_erp_system_user` */
+/*Table structure for table `docu_erp_bill` */
 
-DROP TABLE IF EXISTS `docu_erp_system_user`;
+DROP TABLE IF EXISTS `docu_erp_bill`;
 
-CREATE TABLE `docu_erp_system_user` (
+CREATE TABLE `docu_erp_bill` (
+  `BILL_ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ACCOUNT_ID` char(20) NOT NULL,
   `USER_ID` char(20) NOT NULL,
-  `PASSWORD` char(20) NOT NULL,
-  PRIMARY KEY (`USER_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `ORIGIN_AMOUNT` float NOT NULL,
+  `EXPENSE_AMOUNT` float NOT NULL,
+  `BALANCE` float NOT NULL,
+  `ACTIVITY_ID` bigint(20) NOT NULL,
+  `UPDATE_BY` char(20) NOT NULL,
+  `UPDATE_TIME` datetime NOT NULL,
+  PRIMARY KEY (`BILL_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-/*Data for the table `docu_erp_system_user` */
+/*Data for the table `docu_erp_bill` */
 
-insert  into `docu_erp_system_user`(`USER_ID`,`PASSWORD`) values ('HUMA4','123456');
+insert  into `docu_erp_bill`(`BILL_ID`,`ACCOUNT_ID`,`USER_ID`,`ORIGIN_AMOUNT`,`EXPENSE_AMOUNT`,`BALANCE`,`ACTIVITY_ID`,`UPDATE_BY`,`UPDATE_TIME`) values (1,'1','HUMA4',100,10,90,1,'HUMA4','2015-04-22 09:41:03');
+
+/*Table structure for table `docu_erp_charge` */
+
+DROP TABLE IF EXISTS `docu_erp_charge`;
+
+CREATE TABLE `docu_erp_charge` (
+  `CHARGE_ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `USER_ID` char(20) NOT NULL,
+  `ACCOUNT_ID` char(20) NOT NULL,
+  `RECV_AMOUNT` float NOT NULL,
+  `RECV_TIME` datetime NOT NULL,
+  `PERCENT` int(11) NOT NULL,
+  `UPDATE_BY` char(20) NOT NULL,
+  `UPDATE_TIME` datetime NOT NULL,
+  PRIMARY KEY (`CHARGE_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `docu_erp_charge` */
+
+insert  into `docu_erp_charge`(`CHARGE_ID`,`USER_ID`,`ACCOUNT_ID`,`RECV_AMOUNT`,`RECV_TIME`,`PERCENT`,`UPDATE_BY`,`UPDATE_TIME`) values (1,'HUMA4','1',100,'2015-04-20 22:39:42',20,'HUMA4','2015-04-20 22:39:42'),(2,'ADMIN','2',100,'2015-04-22 12:46:43',20,'ADMIN','2015-04-22 12:46:43'),(3,'HUMA4','1',200,'2015-04-22 13:12:30',20,'HUMA4','2015-04-22 13:12:30'),(4,'HUMA4','1',100,'2015-04-22 17:00:15',20,'HUMA4','2015-04-22 17:00:15');
 
 /*Table structure for table `docu_erp_user` */
 
@@ -57,16 +85,19 @@ DROP TABLE IF EXISTS `docu_erp_user`;
 
 CREATE TABLE `docu_erp_user` (
   `USER_ID` char(20) NOT NULL,
-  `EN_NAME` char(50) NOT NULL,
-  `CN_NAME` char(50) NOT NULL,
+  `USER_NAME` char(50) NOT NULL,
   `TEL_NUMBER` char(11) NOT NULL,
   `IS_ACTIVE` char(1) NOT NULL DEFAULT '1',
+  `IS_ADMIN` char(1) NOT NULL DEFAULT '0',
+  `PASSWORD` char(20) NOT NULL DEFAULT '123456',
+  `UPDATE_BY` char(20) NOT NULL,
+  `UPDATE_TIME` datetime NOT NULL,
   PRIMARY KEY (`USER_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `docu_erp_user` */
 
-insert  into `docu_erp_user`(`USER_ID`,`EN_NAME`,`CN_NAME`,`TEL_NUMBER`,`IS_ACTIVE`) values ('ADMIN','ADMIN','ADMIN','18688187010','1'),('HUMA4','Marquis Hu','HU XINHUI','18688187017','1');
+insert  into `docu_erp_user`(`USER_ID`,`USER_NAME`,`TEL_NUMBER`,`IS_ACTIVE`,`IS_ADMIN`,`PASSWORD`,`UPDATE_BY`,`UPDATE_TIME`) values ('ADMIN','ADMIN','18688187011','1','0','123456','huma4','2015-04-22 17:01:22'),('HUMA4','Marquis Hu','18688187017','1','1','123456','huma4','2015-04-22 18:15:43'),('TEST','TEST','18988187011','0','0','123456','huma4','2015-04-22 17:01:36');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
